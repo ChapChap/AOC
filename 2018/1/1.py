3f91f53e931d7df72b6e66f22375b1fb9db3f53e
antoine.chapusot <antoinechapusot@iMac-de-Antoine.local>
2018-12-24 00:40:38 +0100

    Day One Pytho

---
+++ 2018/1/1.py
@@ -0,0 +1,25 @@
+def first():
+    f = open('input', 'r')
+    f.seek(0)
+    res = 0
+    for line in f:
+        res+=int(line)
+    f.close()
+    print(res)
+
+def second():
+    res = 0
+    lst = set()
+    while True:
+        f = open('input', 'r')
+        for line in f:
+            lst.add(res)
+            res+=int(line)
+            if res in lst:
+                print(res)
+                exit(0)
+        f.close()
+
+
+first()
+second()
